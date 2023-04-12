506da0890a0ed02cf27617dfa9aa6481c36808b6
Thatrandomlurker <dentypacey@gmail.com>
2023-04-12 17:21:38 -0230

    Remove useless code

--- IAModelEditor/GUI/Forms/VARIEditorForm.Designer.cs
+++ IAModelEditor/GUI/Forms/VARIEditorForm.Designer.cs
@@ -30,9 +30,6 @@
         {
             this.VARIEditorPrimitiveList = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.VARIEditorExportButton = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // VARIEditorPrimitiveList
@@ -53,42 +50,11 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Primitives";
             // 
-            // VARIEditorExportButton
-            // 
-            this.VARIEditorExportButton.Location = new System.Drawing.Point(12, 442);
-            this.VARIEditorExportButton.Name = "VARIEditorExportButton";
-            this.VARIEditorExportButton.Size = new System.Drawing.Size(75, 23);
-            this.VARIEditorExportButton.TabIndex = 2;
-            this.VARIEditorExportButton.Text = "Export";
-            this.VARIEditorExportButton.UseVisualStyleBackColor = true;
-            this.VARIEditorExportButton.Click += new System.EventHandler(this.VARIEditorExportButton_OnClick);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(93, 442);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button1";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(174, 442);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button1";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
             // VARIEditorForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(261, 473);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.VARIEditorExportButton);
+            this.ClientSize = new System.Drawing.Size(691, 473);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.VARIEditorPrimitiveList);
             this.Name = "VARIEditorForm";
@@ -103,8 +69,5 @@
 
         private ListBox VARIEditorPrimitiveList;
         private Label label1;
-        private Button VARIEditorExportButton;
-        private Button button2;
-        private Button button3;
     }
 }
